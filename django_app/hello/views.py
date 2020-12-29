78031f840af22e909d8e784ecac3d28534c72f6d
nogawanogawa <t_tsuno_tsuno@yahoo.co.jp>
2020-12-29 22:14:38 +0900

    [add] get url pattern

--- django_app/hello/views.py
+++ django_app/hello/views.py
@@ -3,11 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-def index(request):
-    if 'test' in request.GET:
-        test_query = request.GET['test']
-        result = "Query is " + test_query
-    else:
-        result = "No query parameter"
+def index(request, id, nickname):
+    result = 'your id:' + str(id) + ', name: ' + nickname + '.' 
     return HttpResponse(result)
 
