db409f48aaa9fb9893e5b8753ae1efaaf0f08d61
nogawanogawa <t_tsuno_tsuno@yahoo.co.jp>
2020-12-30 14:03:32 +0900

    [add] connect mysql server

--- django_app/hello/models.py
+++ django_app/hello/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Person(models.Model):
+    name = models.CharField(max_length=100)
+    mail = models.CharField(max_length=100)
+    age = models.IntegerField(default=0)
+
+    def __str__(self):
+        return '<User: ' + str(self.id) + ',' + \
+                self.name + ', ' + self.mail + ', ' + str(self.age) + '>'
